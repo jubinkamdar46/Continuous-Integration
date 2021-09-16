507f02e2697e31f3e6994cd7f6de6188b797a439
Jubin Kamdar <jubinkamdar08@gmail.com>
2021-09-16 15:53:39 +0530

    program for ci

---
+++ pattern.py
@@ -0,0 +1,19 @@
+# Python 3.x code to demonstrate star pattern
+
+
+def pypart(n):
+
+    for i in range(0, n):
+
+
+        for j in range(0, i + 1):
+
+            print("* ", end="")
+
+
+        print("\r")
+
+
+
+n = 6
+pypart(n)
